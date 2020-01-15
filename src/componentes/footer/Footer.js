e92c549f6a5eb11dd428307b8b86fc45e4beeddd
Enrique Lopez <jenriquelopez.dev@gmail.com>
2020-01-15 11:02:49 -0600

    footer

---
+++ src/componentes/footer/Footer.js
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+//import './Slider.css';
+
+class Footer extends React.Component {
+  render() {
+    return <footer className="container"></footer>;
+  }
+}
+
+export default Footer;
