9c73f4aa36c489707e4abd39116af2a56d2b79a6
Enrique Lopez <jenriquelopez.dev@gmail.com>
2020-01-15 11:31:44 -0600

    Table Sales

---
+++ src/componentes/home/sales/Sales.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import MaterialTable from "material-table";
+//import './Slider.css';
+
+class Sales extends React.Component {
+  render() {
+    return <MaterialTable></MaterialTable>;
+  }
+}
+
+export default withRouter(Sales);
