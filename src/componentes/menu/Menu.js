c50860783cd15df7b036ea980023f3ec7615ff8b
Enrique Lopez <elopez@digitalgeko.com>
2020-01-15 11:01:41 -0600

    navbar

---
+++ src/componentes/menu/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import './Menu.css';
+
+class Menu extends React.Component {
+  render() {
+    return <nav className=" navbar-expand-md navbar-dark fixed-top"></nav>;
+  }
+}
+
+export default Menu;
