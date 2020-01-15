c9b35bad5c85c227d0c6c6d74324d18c84bb1ea2
Enrique Lopez <jenriquelopez.dev@gmail.com>
2020-01-15 11:30:01 -0600

    Card home

--- src/componentes/home/Home.js
+++ src/componentes/home/Home.js
@@ -3,13 +3,35 @@ import React from "react";
 import Menu from "../menu/Menu";
 import Footer from "../footer/Footer";
 
+import { Container, Grid, Card } from "@material-ui/core";
+
 class Home extends React.Component {
   render() {
     return (
       <>
         <Menu />
 
-        <main role="main" className="flex-shrink-0 mt-5"></main>
+        <main role="main" className="flex-shrink-0 mt-5">
+          <Container fixed>
+            <Grid container>
+              <Grid item xs={12}>
+                <h1>Ventas</h1>
+              </Grid>
+              <Grid item xs={12}>
+                <p>
+                  Aute pariatur ullamco dolor nisi deserunt id consectetur
+                  proident voluptate sint.
+                </p>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Card>
+                  
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
 
         <Footer />
       </>
