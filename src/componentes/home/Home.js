2d06c5894522583abf335883cd86b53ccb867938
Enrique Lopez <jenriquelopez.dev@gmail.com>
2020-01-15 11:04:07 -0600

    home

---
+++ src/componentes/home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Menu from "../menu/Menu";
+import Footer from "../footer/Footer";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Menu />
+
+        <main role="main" className="flex-shrink-0 mt-5"></main>
+
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Home;
